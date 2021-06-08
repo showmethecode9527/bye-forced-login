13688d20a34baa0a4cad24777ac36c1ab48903c6
Mathink <showmethecode@foxmail.com>
2021-06-08 17:13:33 +0800

    feat: 新增 zhihu 支持; 更新名称、图标;

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# F_CK CSDN
+# Bye Forced Login
 
+Chrome 扩展，去除某些网站的强制登录提示。
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 
-Chrome 扩展，去除某站的强制登录提示。
+![csdn](./imgs/001.png)
+![zhihu](./imgs/002.png)
 
-![login dialog](./imgs/001.png)
+## 支持的网站
+
+1. csdn
+2. zhihu
+
 
 ## 安装/使用
 
--- src/js/content.js
+++ src/js/content.js
@@ -1,42 +1,58 @@
-// 遮罩层选择器
-const MASK_SELECTOR = '.login-mark'
-// 弹出的登录框选择器
-const LOGIN_BOX_SELECTOR = '.login-box'
+const TO_BE_REMOVED_SELECTORS = [
+  // csdn 的强制登录
+  '.login-mark',
+  '.login-box',
+  // zhihu 的强制登录
+  '.Modal-wrapper'
+]
 
 // 观察的节点
 // https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver/MutationObserver
-var TARGET_NODE = document.body
+const TARGET_NODE = document.body
 // 要观察的变更
-var config = {
+const config = {
   attributes: false, // 观察属性变动
   childList: true, // 观察子节点变动: 删除或增加
   subtree: false // 观察后代节点
 }
 
-var callback = (mutationsList, observer) => {
+// 标识是否已经做过强制登录的处理
+// 仅处理一次
+let firstEnforcedLoginDone = false
+
+const callback = (mutationsList, observer) => {
   mutationsList.forEach(mutation => {
-    switch(mutation.type) {
+    switch (mutation.type) {
       case 'childList':
-        removeLoginNodes()
+        if (!firstEnforcedLoginDone) {
+          removeNodes()
+          firstEnforcedLoginDone = true
+        }
         break
     }
   })
 }
 
-var observer = new MutationObserver(callback)
+const observer = new MutationObserver(callback)
 
 observer.observe(TARGET_NODE, config)
 
-function removeLoginNodes () {
-  let el = document.querySelector(MASK_SELECTOR)
-  if (el) {
-    console.log('删除遮罩层')
-    TARGET_NODE.removeChild(el)
-  }
+function removeNodes () {
+  let el
+  TO_BE_REMOVED_SELECTORS.forEach(selector => {
+    el = document.querySelector(selector)
+    if (el) {
+      try {
+        el.parentNode.removeChild(el)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  })
 
-  el = document.querySelector(LOGIN_BOX_SELECTOR)
-  if (el) {
-    console.log('删除登录弹框')
-    TARGET_NODE.removeChild(el)
+  // zhihu 自动弹出的登录, 需要取消溢出隐藏
+  const rootEl = document.documentElement
+  if (rootEl.style.overflow === 'hidden') {
+    rootEl.style.overflow = 'auto'
   }
 }
