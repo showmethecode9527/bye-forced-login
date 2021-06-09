9c8bb8c0d299892af1fe9b205db5cb6bc88db328
wangkai <wangkai@youxuepai.com>
2021-06-09 11:21:00 +0800

    fix: 修复 csdn 页面清除自动登录无效的 bug

--- src/js/content.js
+++ src/js/content.js
@@ -18,16 +18,13 @@ const config = {
 
 // 标识是否已经做过强制登录的处理
 // 仅处理一次
-let firstEnforcedLoginDone = false
+let zhihuFirstEnforcedLoginDone = false
 
 const callback = (mutationsList, observer) => {
   mutationsList.forEach(mutation => {
     switch (mutation.type) {
       case 'childList':
-        if (!firstEnforcedLoginDone) {
-          removeNodes()
-          firstEnforcedLoginDone = true
-        }
+        clearForcedLoginDialog()
         break
     }
   })
@@ -37,22 +34,36 @@ const observer = new MutationObserver(callback)
 
 observer.observe(TARGET_NODE, config)
 
-function removeNodes () {
-  let el
+function clearForcedLoginDialog () {
   TO_BE_REMOVED_SELECTORS.forEach(selector => {
-    el = document.querySelector(selector)
-    if (el) {
-      try {
-        el.parentNode.removeChild(el)
-      } catch (e) {
-        console.log(e)
+    if (selector === '.Modal-wrapper') {
+      // 当前是 zhihu 的, 则只删除一次, 因为用户可能会手动点击登录
+      if (!zhihuFirstEnforcedLoginDone) {
+        removeEl(document.querySelector(selector))
+        zhihuFirstEnforcedLoginDone = true
+
+        // zhihu 自动弹出的登录, 需要取消溢出隐藏
+        const rootEl = document.documentElement
+        if (rootEl.style.overflow === 'hidden') {
+          rootEl.style.overflow = 'auto'
+        }
       }
+    } else {
+      // csdn, 只要发现弹窗就删除
+      // 因为如果用户手动点击登录, 则不会出现弹窗, 而是直接跳转到登录页
+      removeEl(document.querySelector(selector))
     }
   })
+}
 
-  // zhihu 自动弹出的登录, 需要取消溢出隐藏
-  const rootEl = document.documentElement
-  if (rootEl.style.overflow === 'hidden') {
-    rootEl.style.overflow = 'auto'
+function removeEl (el) {
+  let result = false
+  try {
+    el.parentNode.removeChild(el)
+    result = false
+  } catch (e) {
+    console.log(e)
   }
+
+  return result
 }
